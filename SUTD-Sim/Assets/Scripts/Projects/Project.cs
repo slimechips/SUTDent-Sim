3f2053ba4c4601e444a3a8a772666ac4c43fa443
slimechips <jason.chowcs#gmail.com>
2019-05-09 20:40:14 +0800

    Add project class

---
+++ SUTD-Sim/Assets/Scripts/Projects/Project.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Project
+{
+    public string name { get; private set; }
+    public int manaCost { get; private set; }
+    public int pillar { get; private set; }
+    public Dictionary<string, int> requirements { get; private set; }
+    public Diciontary<string, int> rewards { get; private set; }
+    public int points { get; private set; }
+}
