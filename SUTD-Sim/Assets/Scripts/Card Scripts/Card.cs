fdc3f54f41057de1d6b95dba137e8dcfb3f9b495
slimechips <jason.chowcs#gmail.com>
2019-06-27 19:38:19 +0800

    Some additions to scriptable objects

---
+++ SUTD-Sim/Assets/Scripts/Card Scripts/Card.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Project", menuName = "School Project")]
+public abstract class Card : ScriptableObject
+{
+    public string cardName;
+    public int manaCost;
+    public Sprite cardArt;
+
+}
--- SUTD-Sim/Assets/Scripts/Projects/Project.cs
+++ SUTD-Sim/Assets/Scripts/Projects/Project.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Project
+public class Project: Card
 {
-    public string name { get; private set; }
-    public int manaCost { get; private set; }
-    public int pillar { get; private set; }
-    public Dictionary<string, int> requirements { get; private set; }
-    public Diciontary<string, int> rewards { get; private set; }
-    public int points { get; private set; }
+    public int pillar;
+    public Dictionary<string, int> requirements;
+    public Dictionary<string, int> rewards;
+    public int points;
 }
